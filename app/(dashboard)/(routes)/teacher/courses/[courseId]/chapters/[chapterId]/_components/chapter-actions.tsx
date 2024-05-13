53bbb19affec159288cef348dbc4f041f40d9635
zubaer-rahman <zubaer.rahman.cse@gmail.com>
2024-05-13 15:47:26 +0600

    6:19 chapter creation process done

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import ConfirmModal from "@/components/modals/confirm-modal";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
 import { Trash } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 interface ChapterActionsProps {
   disabled: boolean;
@@ -15,19 +20,58 @@ const ChapterActions = ({
   chapterId,
   isPublished,
 }: ChapterActionsProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const onPublish = async () => {
+    try {
+      setIsLoading(true);
+      if (isPublished) {
+        await axios.patch(
+          `/api/courses/${courseId}/chapters/${chapterId}/unpublish`
+        );
+        toast.success("Chapter unpublished");
+      } else {
+        await axios.patch(
+          `/api/courses/${courseId}/chapters/${chapterId}/publish`
+        );
+        toast.success("Chapter published");
+      }
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`);
+      toast.success("Chapter deleted");
+      router.push(`/teacher/courses/${courseId}`);
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <div className="flex items-center gap-x-2">
       <Button
-        onClick={() => {}}
-        disabled={disabled}
+        onClick={onPublish}
+        disabled={disabled || isLoading}
         variant="outline"
         size="sm"
       >
         {isPublished ? "Unpublish" : "Publish"}
       </Button>
-      <Button size="sm">
-        <Trash className="h-4 w-4" />
-      </Button>
+      <ConfirmModal onConfirm={onDelete}>
+        <Button size="sm" disabled={isLoading}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </ConfirmModal>
     </div>
   );
 };
--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -3,10 +3,68 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
-const mux = new Mux({
+const { video } = new Mux({
   tokenId: process.env["MUX_TOKEN_ID"], // This is the default and can be omitted
   tokenSecret: process.env["MUX_TOKEN_SECRET"], // This is the default and can be omitted
 });
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { courseId, chapterId } = params;
+
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+    const ownCourse = await db.course.findUnique({
+      where: { id: courseId, userId },
+    });
+
+    if (!ownCourse) return new NextResponse("Unauthorized", { status: 401 });
+
+    const chapter = await db.chapter.findUnique({
+      where: { id: chapterId, courseId },
+    });
+
+    if (!chapter) return new NextResponse("Not Found", { status: 404 });
+
+    if (chapter.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: {
+          chapterId,
+        },
+      });
+      if (existingMuxData) {
+        await video.assets.delete(existingMuxData.assetId);
+        await db.muxData.delete({
+          where: { id: existingMuxData.id },
+        });
+      }
+    }
+    const deletedChapter = await db.chapter.delete({
+      where: { id: chapterId },
+    });
+
+    const publishedChaptersInCourse = await db.chapter.findMany({
+      where: { courseId, isPublished: true },
+    });
+
+    if (!publishedChaptersInCourse.length) {
+      await db.course.update({
+        where: { id: courseId },
+        data: { isPublished: false },
+      });
+    }
+
+    return NextResponse.json(deletedChapter);
+  } catch (error) {
+    console.log("CHAPTER_ID_DELETE", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
 export async function PATCH(
   req: Request,
   { params }: { params: { courseId: string; chapterId: string } }
@@ -35,16 +93,15 @@ export async function PATCH(
       });
 
       if (existingMuxData) {
-        await mux.video.assets.delete(existingMuxData.assetId);
+        await video.assets.delete(existingMuxData.assetId);
         await db.muxData.delete({
           where: { id: existingMuxData.id },
         });
       }
-      const asset = await mux.video.assets.create({
+      const asset = await video.assets.create({
         input: values.videoUrl,
         playback_policy: ["public"],
         test: false,
-        
       });
       await db.muxData.create({
         data: {
